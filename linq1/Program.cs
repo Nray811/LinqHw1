a0c0d1c3404275332d5b78b97040259ce5bc77f7
Nray <ray08118686@gmail.com>
2023-02-18 21:08:02 +0800

    Linq 1&2

--- linq1/Program.cs
+++ linq1/Program.cs
@@ -21,53 +21,59 @@ namespace linq1
             {
                 sum += i.Price * i.Quantity;
             }
-            Console.WriteLine($"總價格:{sum}");    //第一題
+            Console.WriteLine($"1. 計算所有商品的總價格:{sum}");    
+            Console.WriteLine("");
             decimal average = (sum / list.Count);
-            Console.WriteLine($"平均價格:{average}"); //第二題        
-            Console.WriteLine($"商品總數量{list.Sum(x => x.Quantity)}"); //第三題
-            Console.WriteLine($"商品平均數量:{list.Average(x => x.Quantity):f2}"); //第四題-1
-            Console.WriteLine($"商品平均數量{list.Sum(x => x.Quantity) / list.Count}"); //第四題-2
-            Console.WriteLine($"最貴的商品:{list.Max(x => x.Price)}"); //第五題
-            Console.WriteLine($"最貴的商品:{list.Min(x => x.Price)}"); //第六題
-
+            Console.WriteLine($"2. 計算所有商品的平均價格:{average}"); 
+            Console.WriteLine("");
+            Console.WriteLine($"3. 計算商品的總數量{list.Sum(x => x.Quantity)}"); 
+            Console.WriteLine("");
+            Console.WriteLine($"4. 計算商品的平均數量:{list.Average(x => x.Quantity):f2}"); //第四題-1
+            Console.WriteLine($"5. 找出哪一項商品最貴{list.Sum(x => x.Quantity) / list.Count}"); //第四題-2
+            Console.WriteLine("");
+            Console.WriteLine($"6. 找出哪一項商品最便宜:{list.Max(x => x.Price)}"); 
+            Console.WriteLine("");
+            Console.WriteLine($"7. 計算產品類別為 3C 的商品總價:{list.Min(x => x.Price)}"); 
+            Console.WriteLine("");
             var product_3c = list.Where(x => x.Category == "3C").Sum(x => x.Price);
-            Console.WriteLine($"算產品類別為3C的商品總價:{product_3c}"); //第七題
-
+            Console.WriteLine($"8. 計算產品類別為飲料及食品的商品價格:{product_3c}"); 
+            Console.WriteLine("");
             var porduct_beverage= list.Where((x) => x.Category == "飲料" ||  x.Category == "食品").Sum(x => x.Price);
-            Console.WriteLine($"算產品類別為飲料及食品3C的商品總價:{porduct_beverage}");  //第八題
+            Console.WriteLine($"9. 找出所有商品類別為食品，而且商品數量大於 100 的商品:{porduct_beverage}");  
+            Console.WriteLine("");
             var product_food = list.Where((x) => x.Category == "食品" || x.Quantity > 100);
-            Console.WriteLine("找出所有商品類別為食品，而且商品數量大於 100 的商品");  //第九題
+            Console.WriteLine("10. 找出各個商品類別底下有哪些商品的價格是大於 1000 的商品:");  
             foreach (var i in product_food)
             {
                 Console.WriteLine($"{i.Category} {i.Name} {i.Quantity}");
             }
-
+            Console.WriteLine("");
             var product_1000 = list.Where((x) => x.Price > 1000).GroupBy((x) => x.Category);
-            Console.WriteLine("找出各個商品類別底下有哪些商品的價格是大於 1000 的商品"); //第十題
+            Console.WriteLine("11. 呈上題，請計算該類別底下所有商品的平均價格"); 
             foreach (var i in product_1000)
             {
                 foreach (var j in i)
                     {
                     Console.WriteLine($"{j.Category} {j.Price}");
                 }               
-                Console.WriteLine($"{i.Average((x) => x.Price)}"); //第十一題
+                Console.WriteLine($"{i.Average((x) => x.Price)}"); 
             }
-
-            Console.WriteLine("依照商品價格由高到低排序"); //第十二題
+            Console.WriteLine("");
+            Console.WriteLine("12. 依照商品價格由高到低排序:");
             list.OrderByDescending(x => x.Price);
             foreach (var i in list)
             {
                 Console.WriteLine($"{i.Name} {i.Price}");
-            }          
-            Console.WriteLine("依照商品數量由低到高排序"); //第十三題
+            }
+            Console.WriteLine("");
+            Console.WriteLine("13. 依照商品數量由低到高排序:"); 
             list.OrderBy(x => x.Quantity);
             foreach (var i in list)
             {
                 Console.WriteLine($"{i.Name} {i.Quantity}");
-            }
-            
+            }          
             Console.WriteLine("");
-            Console.WriteLine("找出各商品類別底下，最貴的商品");  //第十四題
+            Console.WriteLine("14. 找出各商品類別底下，最貴的商品:"); 
             var expensive = list.GroupBy((x) => x.Category);
             foreach (var i in expensive)
             { 
@@ -81,7 +87,7 @@ namespace linq1
             }
             
             Console.WriteLine("");
-            Console.WriteLine("找出各商品類別底下，最便宜的商品");  //第十五題
+            Console.WriteLine("15. 找出各商品類別底下，最便宜的商品:");  
             var cheap = list.GroupBy((x) => x.Category);
             foreach (var i in expensive)
             {
@@ -93,8 +99,26 @@ namespace linq1
 
                 }
             }
+            Console.WriteLine("16. 找出價格小於等於 10000 的商品:");
+            var priceless_10000 = list.Where(x => x.Price <= 10000);
+            foreach (var p in priceless_10000)
+            {
+                Console.WriteLine($"{p.Name} {p.Price}");
 
+            }
 
+            Console.WriteLine("17. 製作一頁 4 筆總共 5 頁的分頁選擇器:");
+            Console.WriteLine("想看1~5頁的哪一頁? \n請輸入數字1~5喔");
+            string page = Console.ReadLine();
+            var pages = list.Skip((int.Parse(page)-1) * 4).Take(4).ToList();          
+            foreach(var p in pages)
+            {              
+                if ( page!="0" )
+                {
+                    Console.WriteLine($"{p.Po}  {p.Name} {p.Quantity} {p.Price} {p.Category}");
+                }                             
+            }
+            
 
 
         }
@@ -115,7 +139,7 @@ namespace linq1
                 {
                     list.Add(new Product { Po = values[0], Name = values[1], Quantity = int.Parse(values[2]), Price = Decimal.Parse(values[3]), Category = values[4] });
                 }
-                Console.WriteLine(values[0]);
+               
             }
             return list;
            
---
+++ linq2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Random random = new Random();
+                int[] gm = Enumerable.Range(1, 9).OrderBy(x => random.Next()).Take(4).ToArray();   //生成亂數，拿四個到int array裡面
+                Console.WriteLine("猜4個數字!");
+                // Console.WriteLine($"電腦選號為:{gm[0]}{gm[1]}{gm[2]}{gm[3]}");   //電腦選號結果
+                while (true)
+                {
+                    Console.WriteLine("猜一波唄");
+                    string input = Console.ReadLine();
+                    if (input.Length == 4)
+                    {
+
+                        int[] guess = input.Select(x => int.Parse(x.ToString())).ToArray();  //把輸入的字串轉數字再存進int array裡面
+                        int a = gm.Where((i, j) => guess[j] == i).Count(); //位置完全正確的
+                        int b = gm.Intersect(guess).Count() - a; //差集扣掉位置完全正確的
+                        if (a == 4)
+                        {
+                            Console.WriteLine("你終於猜對啦");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{a}A{b}B");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("只能輸入四個數字!");
+                        continue;
+                    }
+                }
+                Console.WriteLine("你要繼續玩嗎？ yes to continue");
+            } while (Console.ReadLine() == "yes");
+
+        }
+    }
+}
+
