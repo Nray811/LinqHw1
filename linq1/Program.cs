0f726598277734aaf9c824290137a9f052a03399
Nray <ray08118686@gmail.com>
2023-02-18 21:20:43 +0800

    update

--- linq1/Program.cs
+++ linq1/Program.cs
@@ -28,35 +28,37 @@ namespace linq1
             Console.WriteLine("");
             Console.WriteLine($"3. 計算商品的總數量{list.Sum(x => x.Quantity)}"); 
             Console.WriteLine("");
-            Console.WriteLine($"4. 計算商品的平均數量:{list.Average(x => x.Quantity):f2}"); //第四題-1
-            Console.WriteLine($"5. 找出哪一項商品最貴{list.Sum(x => x.Quantity) / list.Count}"); //第四題-2
+            Console.WriteLine($"4.1 計算商品的平均數量:{list.Average(x => x.Quantity):f2}"); //第四題-1
             Console.WriteLine("");
-            Console.WriteLine($"6. 找出哪一項商品最便宜:{list.Max(x => x.Price)}"); 
+            Console.WriteLine($"4.2 計算商品的平均數量{list.Sum(x => x.Quantity) / list.Count}"); //第四題-2
             Console.WriteLine("");
-            Console.WriteLine($"7. 計算產品類別為 3C 的商品總價:{list.Min(x => x.Price)}"); 
+            Console.WriteLine($"5. 找出哪一項商品最貴:{list.Max(x => x.Price)}"); 
+            Console.WriteLine("");
+            Console.WriteLine($"6. 找出哪一項商品最便宜:{list.Min(x => x.Price)}"); 
             Console.WriteLine("");
             var product_3c = list.Where(x => x.Category == "3C").Sum(x => x.Price);
-            Console.WriteLine($"8. 計算產品類別為飲料及食品的商品價格:{product_3c}"); 
+            Console.WriteLine($"7. 計算產品類別為 3C 的商品總價:{product_3c}"); 
             Console.WriteLine("");
             var porduct_beverage= list.Where((x) => x.Category == "飲料" ||  x.Category == "食品").Sum(x => x.Price);
-            Console.WriteLine($"9. 找出所有商品類別為食品，而且商品數量大於 100 的商品:{porduct_beverage}");  
+            Console.WriteLine($"8. 計算產品類別為飲料及食品的商品價格:{porduct_beverage}");  
             Console.WriteLine("");
             var product_food = list.Where((x) => x.Category == "食品" || x.Quantity > 100);
-            Console.WriteLine("10. 找出各個商品類別底下有哪些商品的價格是大於 1000 的商品:");  
+            Console.WriteLine("9. 找出所有商品類別為食品，而且商品數量大於 100 的商品:");  
             foreach (var i in product_food)
             {
                 Console.WriteLine($"{i.Category} {i.Name} {i.Quantity}");
             }
             Console.WriteLine("");
             var product_1000 = list.Where((x) => x.Price > 1000).GroupBy((x) => x.Category);
-            Console.WriteLine("11. 呈上題，請計算該類別底下所有商品的平均價格"); 
+            Console.WriteLine("10. 找出各個商品類別底下有哪些商品的價格是大於 1000 的商品:"); 
             foreach (var i in product_1000)
             {
                 foreach (var j in i)
                     {
                     Console.WriteLine($"{j.Category} {j.Price}");
-                }               
-                Console.WriteLine($"{i.Average((x) => x.Price)}"); 
+                }
+                Console.WriteLine("");
+                Console.WriteLine($"11. 呈上題，請計算該類別底下所有商品的平均價格:{i.Average((x) => x.Price)}"); 
             }
             Console.WriteLine("");
             Console.WriteLine("12. 依照商品價格由高到低排序:");
@@ -66,18 +68,19 @@ namespace linq1
                 Console.WriteLine($"{i.Name} {i.Price}");
             }
             Console.WriteLine("");
-            Console.WriteLine("13. 依照商品數量由低到高排序:"); 
+            Console.WriteLine("13. 依照商品數量由低到高排序:");
             list.OrderBy(x => x.Quantity);
             foreach (var i in list)
             {
                 Console.WriteLine($"{i.Name} {i.Quantity}");
-            }          
+            }
+            
             Console.WriteLine("");
-            Console.WriteLine("14. 找出各商品類別底下，最貴的商品:"); 
+            Console.WriteLine("14. 找出各商品類別底下，最貴的商品:");
             var expensive = list.GroupBy((x) => x.Category);
             foreach (var i in expensive)
             { 
-                Console.WriteLine($"{i.Key} 最貴的商品:");
+                Console.WriteLine($"{i.Key}");
                 var richest = i.Where(x => x.Price == (i.Max(y => y.Price)));  
                 foreach (var r in richest)
                 {
@@ -99,14 +102,15 @@ namespace linq1
 
                 }
             }
-            Console.WriteLine("16. 找出價格小於等於 10000 的商品:");
+            Console.WriteLine("");
+            Console.WriteLine("16. 找出價格小於等於10000 的商品:");
             var priceless_10000 = list.Where(x => x.Price <= 10000);
             foreach (var p in priceless_10000)
             {
                 Console.WriteLine($"{p.Name} {p.Price}");
 
             }
-
+            Console.WriteLine("");
             Console.WriteLine("17. 製作一頁 4 筆總共 5 頁的分頁選擇器:");
             Console.WriteLine("想看1~5頁的哪一頁? \n請輸入數字1~5喔");
             string page = Console.ReadLine();
